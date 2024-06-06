c44b45a0fdb8340b85ab8df99453b3385cecf594
kirankhanalleo <iamkirankhanal@gmail.com>
2024-06-06 14:50:15 +0545

    created tree view in createform component for using roles in access group

--- src/pages/AccessGroup/CreateAccessGroup/CreateAccessGroup.jsx
+++ src/pages/AccessGroup/CreateAccessGroup/CreateAccessGroup.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import "./CreateAccessGroup.scss"
+import React from 'react';
+import "./CreateAccessGroup.scss";
 import CustomForm from '../../../components/CustomForm/CustomForm';
+
 const CreateAccessGroup = () => {
-    const fields = [
-        { name: 'name', type: 'text' },
-        { name: 'description', type: 'textarea' },
-        { name: 'type', type: 'select', 
-          options: 
-          [
-            { label: 'Vendor', value: 'vendor' }, 
-            { label: 'User', value: 'user' }
-          ] 
-        }
-    ];
+  const fields = [
+    { name: 'name', type: 'text' },
+    { name: 'description', type: 'textarea' },
+    { name: 'type', type: 'select', 
+      options: [
+        { label: 'Group1', value: 'group1' }, 
+        { label: 'Group2', value: 'group2' },
+        { label: 'Group3', value: 'group3' },
+      ] 
+    }
+  ];
+
   return (
     <div>
-      <CustomForm header="Create Access Group" fields={fields} />
+      <CustomForm header="Create Access Group" fields={fields} flexDirection='column' />
     </div>
-  )
+  );
 }
 
-export default CreateAccessGroup
+export default CreateAccessGroup;
